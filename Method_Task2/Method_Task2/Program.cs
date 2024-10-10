4f5fa92ecc2934a31ce7a92ad8f09a50079c814a
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-10-10 10:23:31 +0400

    tasks added

---
+++ Method_Task2/Method_Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Method_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("area of rectangle: "+Area(5, 6));
+            Console.WriteLine("Area of paralelopiped: " + Area(5, 6,2));
+            Console.WriteLine("Area of circle: " + Area(5));
+            Console.WriteLine("Area of circle in triangle: " + Area(4,5, 6, 2));
+
+
+        }
+        public static int Area(int a, int b)
+        {
+            int s = a * b;
+            return s;
+        }
+        public static int Area(int a, int b,int c)
+        {
+            int s = 2*(a*b+a*c+b*c);
+            return s;
+        }
+        public static int Area(int r)
+        {
+            int pi = 3;
+            int s =pi*(r*r) ;
+            return s;
+        }
+        public static int Area(int r, int a, int b, int c)
+        {
+            int p = (a+b+c)/2;
+            int s = p*r;
+            return s;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿// Task1
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 5, 1, 4, 6, 8, 3 };
+            Console.WriteLine(GetMin(arr));
+        }
+
+        public static int GetMin(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+    }
+}
+
+
+
+
+
